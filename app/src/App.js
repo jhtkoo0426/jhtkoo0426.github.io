99570f08379135e7a5293d08a83f8e8aea3d762f
jhtkoo0426 <jhtkoo@gmail.com>
2023-09-26 15:50:40 +0100

    Changed app title and icon

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import React, { useRef, useState, useEffect } from "react";
 
 
@@ -43,8 +43,8 @@ export default function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRoute />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
