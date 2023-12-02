28ae45517a21cbadaaf614a0aeca95443df04819
Justin Koo <jhtkoo@gmail.com>
2023-12-02 15:47:28 +0000

    Completed temporal styling for landing homepage

--- app/src/components/Title.js
+++ app/src/components/Title.js
@@ -1,42 +1,21 @@
 // React.js component for a variable-size titlype.
+import { text } from "@fortawesome/fontawesome-svg-core";
 import { motion } from "framer-motion";
 
 
 // size (options: huge, large, standard, tiny): Choose from a pre-defined font size.
 // is_transparent (bool): Conditionally apply styling for the title.
-// font (str, options: playfair, inter): Choose from a list of fonts. 
-// animated (bool): If true, uses framer motion variants.
-// with_top (bool): Optionally add a design on top of the title
-// color (str, options: theme, darkgrey)
-const Title = ({ id, children, size, is_transparent, font, animated, with_top, top_color }) => {
-    if (top_color === undefined || top_color === null) {
-        top_color = 'darkgrey';
+// font (str | options: playfair, inter, interbold): Choose from a list of fonts.
+// text_transform (str | option: upper, lower, normal. default = normal)
+const Title = ({ id, className, children, size, is_transparent, font, text_transform}) => {
+    if (text_transform === undefined || text_transform === null) {
+        text_transform = 'normal';
     }
-
-    return animated ?
-        <motion.h1
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            transition={{ duration: 0.3 }}
-            variants={{
-                visible: { opacity: 1, x: 0 },
-                hidden: { opacity: 0, x: -200 }
-            }}
-            className={`title ${size === undefined ? 'large' : size} ${font}` + (is_transparent === true ? " transparent" : "") }
-            id={id}
-        >
+    return (
+        <h1 className={`title ${className} ${size === undefined ? 'large' : size} ${font} ${text_transform}` + (is_transparent === true ? " transparent" : "") } id={id}>
             { children }
-        </motion.h1> : (
-            with_top ?
-            <h1 className={`title ${size === undefined ? 'large' : size} ${top_color} ${font}` + (is_transparent === true ? " transparent" : "") } id={id}>
-                <div className="title-top"></div>
-                { children }
-            </h1> :
-            <h1 className={`title ${size === undefined ? 'large' : size} ${top_color} ${font}` + (is_transparent === true ? " transparent" : "") } id={id}>
-                { children }
-            </h1>
-        )
+        </h1>
+    )
 }
 
 export default Title;
--- app/src/pages/Layout.js
+++ app/src/pages/Layout.js
@@ -22,59 +22,55 @@ import Lenis from '@studio-freight/lenis'
 
 
 const Layout = () => {
-  const lenis = new Lenis()
-  
-  lenis.on('scroll', (e) => {
-      console.log(e)
-  })
-  
-  function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-  }
-  
-  requestAnimationFrame(raf)
-  return (
-    <>
-      <motion.nav className="topnav" initial={{ opacity: 0, y: -50}} animate={{ opacity: 1, y: 0, transition: { delay: 0.25} }}>
-        <div className="home-button">
-          <Link to={"/"}>
-            <p className="name">logo</p>
-          </Link>
-        </div>
-        <div className="topnav-menu">
-          <NavLink className="topnav-link anchor underline" activeclassname="active" to="/">home</NavLink>
-          <NavLink className="topnav-link anchor underline" activeclassname="active" to="/work">work</NavLink>
-          <NavLink className="topnav-link anchor underline" activeclassname="active" to="/files/justin_koo_resume_2023.pdf" target="_blank" download>Résumé</NavLink>
-        </div>
-      </motion.nav>
-      <div className="leftnav">
-        <div className='social-links'>
-          <Anchor target={true} underline={false} href={"https://www.linkedin.com/in/koo-justin/"}>
-            <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-          </Anchor>
-          <Anchor target={true} underline={false} href={"https://github.com/jhtkoo0426"}>
-            <FontAwesomeIcon icon={faLinkedinIn}></FontAwesomeIcon>
-          </Anchor>
-          <Anchor target={true} underline={false} href={"https://www.kaggle.com/jhtkoo0426"}>
-            <FontAwesomeIcon icon={faKaggle}></FontAwesomeIcon>
-          </Anchor>
-          {/* <Anchor href={"mailto:jhtkbusiness@gmail.com"} target={true} underline={false}>
-            <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
-          </Anchor> */}
-        </div>
-      </div>
-      <div className="rightnav">
-      </div>
-      <div className="app-container">
-        {/* Container for the main content of the portfolio */}
-        <div className="main-container"><Outlet /></div>
-      </div>
-      <div className='botnav-placehold'>
-        
-      </div>
-    </>
-  );
+    // Apply smooth parallax scroll with Lenis
+    const lenis = new Lenis()
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+
+    return (
+        <>
+          <motion.nav className="topnav" initial={{ opacity: 0, y: -50}} animate={{ opacity: 1, y: 0, transition: { delay: 0.25} }}>
+            <div className="home-button">
+              <Link to={"/"}>
+                <p className="name">logo</p>
+              </Link>
+            </div>
+            <div className="topnav-menu">
+              <NavLink className="topnav-link anchor underline" activeclassname="active" to="/">home</NavLink>
+              <NavLink className="topnav-link anchor underline" activeclassname="active" to="/work">work</NavLink>
+              <NavLink className="topnav-link anchor underline" activeclassname="active" to="/files/justin_koo_resume_2023.pdf" target="_blank" download>Résumé</NavLink>
+            </div>
+          </motion.nav>
+          <div className="leftnav">
+            <div className='social-links'>
+              <Anchor target={true} underline={false} href={"https://www.linkedin.com/in/koo-justin/"}>
+                <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+              </Anchor>
+              <Anchor target={true} underline={false} href={"https://github.com/jhtkoo0426"}>
+                <FontAwesomeIcon icon={faLinkedinIn}></FontAwesomeIcon>
+              </Anchor>
+              <Anchor target={true} underline={false} href={"https://www.kaggle.com/jhtkoo0426"}>
+                <FontAwesomeIcon icon={faKaggle}></FontAwesomeIcon>
+              </Anchor>
+              {/* <Anchor href={"mailto:jhtkbusiness@gmail.com"} target={true} underline={false}>
+                <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+              </Anchor> */}
+            </div>
+          </div>
+          <div className="rightnav">
+          </div>
+          <div className="app-container">
+            {/* Container for the main content of the portfolio */}
+            <div className="main-container"><Outlet /></div>
+          </div>
+          <div className='botnav-placehold'>
+            
+          </div>
+        </>
+    );
 };
 
 export default Layout;
