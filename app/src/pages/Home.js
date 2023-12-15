02461ec83a463bbe2b16934de8b46f878bb81a2b
Justin Koo <jhtkoo@gmail.com>
2023-12-15 18:09:58 +0000

    Updated homepage project navigation

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,10 +1,8 @@
 // Home.js template
 // Import core libraries
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 // Import components
-import Title from "../components/Title";
 import ProjectLink from '../components/ProjectLink';
 
 
@@ -13,14 +11,15 @@ const Home = () => {
         <div className='homepage'>
             <div className='projects-nav'>
                 <ProjectLink year={2023} project_title={'zephyr'}></ProjectLink>
-                <ProjectLink year={2023} project_title={'ucl diss.'}></ProjectLink>
-                <ProjectLink year={2023} project_title={'epidemic simulation'}></ProjectLink>
+                <ProjectLink year={2023} project_title={'tube planner'}></ProjectLink>
                 <ProjectLink year={2023} project_title={'london qol analysis'}></ProjectLink>
                 <ProjectLink year={2023} project_title={'housing price prediction'}></ProjectLink>
-                <ProjectLink year={2023} project_title={'autofaq'}></ProjectLink>
-                <ProjectLink year={2023} project_title={'tube planner'}></ProjectLink>
-                <ProjectLink year={2023} project_title={'tetris ai'}></ProjectLink>
-                <ProjectLink year={2023} project_title={'l4s school analysis'}></ProjectLink>
+                <ProjectLink year={2023} project_title={'epidemic simulation'}></ProjectLink>
+                <ProjectLink year={2023} project_title={'ucl diss.'}></ProjectLink>
+                <ProjectLink year={2022} project_title={'autofaq'}></ProjectLink>
+                <ProjectLink year={2020} project_title={'tetris ai'}></ProjectLink>
+                <ProjectLink year={2020} project_title={'l4s school analysis'}></ProjectLink>
+                <ProjectLink year={2020} project_title={'journey begins'}></ProjectLink>
             </div>
         </div>
     );
